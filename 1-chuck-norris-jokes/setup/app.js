0bd99838052e438885439aac90c32603f4d85e5a
Edwin Alexander Cárdenas <eacardenase@unal.edu.co>
2021-10-29 12:51:54 -0500

    random jokes v4 fetch

--- 1-chuck-norris-jokes/setup/app.js
+++ 1-chuck-norris-jokes/setup/app.js
@@ -4,34 +4,15 @@ const img = document.querySelector(".container img");
 const URL = "https://api.chucknorris.io/jokes/random";
 
 button.addEventListener("click", () => {
-    getData(URL)
+    fetch(URL)
+        .then((data) => data.json())
         .then((response) => displayData(response))
         .catch((error) => console.log(error));
 });
 
-function getData(url) {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open("GET", url);
-        xhr.send();
-        xhr.onreadystatechange = function () {
-            if (this.readyState !== 4) return;
-            if (this.status === 200) {
-                resolve(this.responseText);
-            } else {
-                reject({
-                    status: this.status,
-                    text: this.statusText,
-                });
-            }
-        };
-    });
-}
-
-function displayData(data) {
+function displayData({ value: randomJoke }) {
     // shake image
     img.classList.add("shake-img");
-    const { value: randomJoke } = JSON.parse(data);
     content.innerHTML = randomJoke;
     // setting the time randomly
     const random = Math.floor(Math.random() * 1000);
