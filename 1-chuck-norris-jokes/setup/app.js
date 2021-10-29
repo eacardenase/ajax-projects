09c5b2c62bb47cdf5a942658dfff41115e78ec30
Edwin Alexander Cárdenas <eacardenase@unal.edu.co>
2021-10-29 12:30:25 -0500

    random jokes v3 promises

--- 1-chuck-norris-jokes/setup/app.js
+++ 1-chuck-norris-jokes/setup/app.js
@@ -4,30 +4,38 @@ const img = document.querySelector(".container img");
 const URL = "https://api.chucknorris.io/jokes/random";
 
 button.addEventListener("click", () => {
-    getRandomJoke(URL);
+    getData(URL)
+        .then((response) => displayData(response))
+        .catch((error) => console.log(error));
 });
 
-function getRandomJoke(url) {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
-    xhr.send();
-    xhr.onreadystatechange = function () {
-        if (this.readyState !== 4) return;
-        if (this.status === 200) {
-            // shake image
-            img.classList.add("shake-img");
-            const { value: randomJoke } = JSON.parse(this.responseText);
-            content.innerHTML = randomJoke;
-            // setting the time randomly
-            const random = Math.floor(Math.random() * 1000);
-            setTimeout(() => {
-                img.classList.remove("shake-img");
-            }, random);
-        } else {
-            console.log({
-                status: this.status,
-                text: this.statusText,
-            });
-        }
-    };
+function getData(url) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+        xhr.send();
+        xhr.onreadystatechange = function () {
+            if (this.readyState !== 4) return;
+            if (this.status === 200) {
+                resolve(this.responseText);
+            } else {
+                reject({
+                    status: this.status,
+                    text: this.statusText,
+                });
+            }
+        };
+    });
+}
+
+function displayData(data) {
+    // shake image
+    img.classList.add("shake-img");
+    const { value: randomJoke } = JSON.parse(data);
+    content.innerHTML = randomJoke;
+    // setting the time randomly
+    const random = Math.floor(Math.random() * 1000);
+    setTimeout(() => {
+        img.classList.remove("shake-img");
+    }, random);
 }
